c1f53f7bac829b26a52c765c554133d92be05259
SMazeikis <salvijus.mazeikis2@mail.dcu.ie>
2021-02-22 00:17:27 -0800

    Category Selection screen created

---
+++ src/FrontEnd/src/views/CategorySelection.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-list>
+      <ion-item>
+      <ion-button @click="openModal">Active Life</ion-button>
+      </ion-item>
+      <ion-item>
+      <ion-button @click="openModal">Local Flavor</ion-button>
+      </ion-item>
+      <ion-item>
+      <ion-button @click="openModal">NightLife</ion-button>
+      </ion-item>
+      <ion-item>
+      <ion-button @click="openModal">Restaurants</ion-button>
+      </ion-item>
+      <ion-item>
+      <ion-button @click="openModal">Arts & Entertainment</ion-button>
+      </ion-item>
+    </ion-list>
+  </ion-page>
+</template>
+
+
+<script>
+import { IonButton, IonPage, modalController, IonList, IonItem } from '@ionic/vue';
+import Modal from './Modal.vue'
+
+export default {
+  components: { IonButton, IonPage, IonList, IonItem },
+  methods: {
+    async openModal() {
+      const modal = await modalController
+        .create({
+          component: Modal,
+          cssClass: 'my-custom-class',
+          componentProps: {
+            title: 'New Title'
+          },
+        })
+      return modal.present();
+    },
+  },
+}
+</script>
---
+++ src/FrontEnd/src/views/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-grid>
+      <ion-item>
+      <ion-button class="Button" >Something</ion-button>
+      <ion-button class="Button" >Something</ion-button>
+      <ion-button class="Button" >Something</ion-button>
+      </ion-item>
+      <ion-item>
+      <ion-button class="Button" >Something</ion-button>
+      <ion-button class="Button" >Something</ion-button>
+      </ion-item>
+    </ion-grid>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonGrid,
+  IonItem,
+  IonButton,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+  props: {
+    title: { type: String, default: "Super Modal" },
+  },
+  data() {
+    return {
+      content: "Content",
+    };
+  },
+  created() {
+    console.log("here")
+  },
+  components: { IonPage, IonGrid, IonItem, IonButton, },
+});
+</script>
+
+<style scoped>
+.Button {
+  display: block;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  border: 1px solid red;
+}
+</style>
