b697fa20580390998ac3066636032c53aef0f99e
Bozhidar <bozhidar.sabahov2@mail.dcu.ie>
2021-04-08 16:03:24 +0100

    Connect YELP and other back-end changes

--- src/back-end/app/__init__.py
+++ src/back-end/app/__init__.py
@@ -4,15 +4,15 @@ import firebase_admin
 from firebase_admin import firestore
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
 
+CORS(app)
+
 firebase_admin.initialize_app()
 db = firestore.client()
 
-from app import app
+from app import views
--- src/back-end/app/views.py
+++ src/back-end/app/views.py
@@ -1,17 +1,34 @@
 from app import app
 from flask import request
 from flask_cors import cross_origin
-from user import assignCategories
+from .user import assignCategories
+from yelp_api.businesses import YelpGQL
+
+import requests
 
 
 @app.route('/categorySelection', methods=['GET', 'POST'])
 @cross_origin()
 def userCategories():
-    try:
+    if request.is_json:
         data = request.get_json()
         print(data)
         d = assignCategories(data)
         return d
-    except Exception as e:
-        print(e)
-        return "not assigned"
+    else:
+        return "Request was not JSON", 400
+
+
+@app.route('/recommender', methods=['GET', 'POST'])
+@cross_origin()
+def recommender():
+    user_id = request.args.get('user_id')
+    recommendations = requests.get(
+            "http://127.0.0.1:5001/recommendations",
+            params={'user_id': user_id}).json()
+
+    yelp = YelpGQL()
+
+    for key in recommendations.keys():
+        yelp.get_business_info(key)
+    return recommendations
