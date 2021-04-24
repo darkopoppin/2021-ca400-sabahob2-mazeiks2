8cc6f4310fa03d6b2702a3e122c8e034e923ab61
Bozhidar <bozhidar.sabahov2@mail.dcu.ie>
2021-04-24 14:50:50 +0100

    Refactor main_service

--- src/back-end/main_service/views.py
+++ src/back-end/main_service/views.py
@@ -40,21 +40,21 @@ def recommender():
     if redis_client.get(user_id) is not None:
         string = redis_client.get(user_id).decode('UTF-8')
         recommendations_ids = string.split(' ')
-    else:
-        response = requests.get(
-                f'http://{settings.RECOMM_HOST}:5001/recommendations',
-                params={'user_id': user_id})
-        if response.status_code != 200:
-            return (
-                response.content,
-                response.status_code,
-                response.headers.items())
-        else:
-            recommendations = response.json()
+        results = get_businesses_info(recommendations_ids)
+        return results
 
-        recommendations_ids = list(recommendations)    
-        redis_client.set(user_id, ' '.join(recommendations_ids))
-        redis_client.expire(user_id, 60*60*24)
+    response = requests.get(
+            f'http://{settings.RECOMM_HOST}:5001/recommendations',
+            params={'user_id': user_id})
+
+    if response.status_code != 200:
+        return (response.content, response.status_code, response.headers.items())
+
+    recommendations = response.json()
+    recommendations_ids = list(recommendations)
+
+    redis_client.set(user_id, ' '.join(recommendations_ids))
+    redis_client.expire(user_id, 60*60*24)
 
     results = get_businesses_info(recommendations_ids)
     return results
