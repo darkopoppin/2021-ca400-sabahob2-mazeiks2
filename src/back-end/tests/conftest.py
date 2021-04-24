f4ba1221960f2756ceee069e8ec8d322716674fd
Bozhidar <bozhidar.sabahov2@mail.dcu.ie>
2021-04-24 07:34:37 +0100

    Fix conftest issue

--- src/back-end/tests/conftest.py
+++ src/back-end/tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import firebase_admin
 
-from main_service import create_service, db
+from main_service import create_service
+from firebase_admin import firestore
 
 
 @pytest.fixture(scope='session')
@@ -15,5 +16,7 @@ def test_client():
 
 @pytest.fixture(scope='session')
 def test_db():
+    app = firebase_admin.initialize_app()
+    db = firestore.client(app)
     yield db
-    firebase_admin.delete_app(db)
+    firebase_admin.delete_app(app)
