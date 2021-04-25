affd491725e60351e6e4a52d32536449aa9cb24d
Bozhidar <bozhidar.sabahov2@mail.dcu.ie>
2021-04-25 10:23:53 +0100

    Add redis to docker compose

--- src/back-end/settings.py
+++ src/back-end/settings.py
@@ -1,12 +1,12 @@
 from os import environ
 
-REDIS_PASS = environ.get('REDIS')
+REDIS_PASS = environ.get('REDIS_PASS')
 if environ.get('FLASK_ENV') == 'development':
     REDIS_HOST = 'localhost'
     REDIS_PORT = '5005'
-    HOST = '127.0.0.1'
-
-# add for testing and production
+else:
+    REDIS_HOST = 'redis-server'
+    REDIS_PORT = '6379'
 
 if environ.get('FLASK_ENV') == 'development':
     RECOMM_HOST = '127.0.0.1'
