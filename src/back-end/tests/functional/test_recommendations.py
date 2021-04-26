6ea7f125d3dfb1d7980b74acae9688dd3167233c
Bozhidar <bozhidar.sabahov2@mail.dcu.ie>
2021-04-26 13:05:42 +0100

    Add staging tests backend

---
+++ src/back-end/tests/functional/test_recommendations.py
@@ -0,0 +1,38 @@
+
+
+def test_new_user(test_client, test_db, test_redis):
+    user_id = '9ENVClSVvVUIsJ9qSguUAjuM5213'
+
+    before_cache = obtain_recommendations(user_id, test_client, test_db)
+    compare_result_and_cached(user_id, before_cache, test_client, test_redis)
+
+
+def test_existing_user(test_client, test_db, test_redis):
+    user_id = 'RS9xp8QTvREUKbsxHoWt'
+
+    before_cache = obtain_recommendations(user_id, test_client, test_db)
+    compare_result_and_cached(user_id, before_cache, test_client, test_redis)
+
+
+def obtain_recommendations(user_id, test_client, test_db):
+    data = dict(user_id=user_id)
+    response = test_client.get('/recommender', query_string=data)
+    assert response.status_code == 200
+    for key, business in response.json.items():
+        assert key.find('b') == 0
+        assert 'name' in business.keys()
+        assert 'id' in business.keys()
+        assert 'location' in business.keys()
+        assert 'rating' in business.keys()
+
+    return response.json
+
+
+def compare_result_and_cached(user_id, before_cache, test_client, test_redis):
+    assert test_redis.get(user_id) is not None
+    data = dict(user_id=user_id)
+    response = test_client.get('/recommender', query_string=data)
+
+    assert len(before_cache) == len(response.json)
+    for old, cached in zip(before_cache.keys(), response.json.keys()):
+        assert old == cached
